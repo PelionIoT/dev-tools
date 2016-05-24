a3484a2e3ef6995360367d03b12f6c8375e8ed10
Ed Hemphill <edhemphill@gmail.com>
2016-05-24 14:46:18 -0500

    Update push-rsync.sh

--- bin/push-rsync.sh
+++ bin/push-rsync.sh
@@ -5,7 +5,7 @@ SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   SELF="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$SELF/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 
 MYDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
